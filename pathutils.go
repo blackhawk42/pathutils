09260ad19d078c820e1191382002bbb9343bdc98
Daniel Chávez <danielsaul96@gmail.com>
2017-10-10 20:45:13 -0500

    Adapted to stupid isNotExist behaviour and added verbose fucking error handling. Fuck Go

--- pathutils.go
+++ pathutils.go
@@ -8,12 +8,18 @@ import(
 )
 
 // Golang: "A simple 'isFile' or 'FileExists' function? Fuck that, you filthy causual!"
-func FileExists(filename string) bool {
-	if _, err := os.Stat(filename); !os.IsNotExist(err){
-		return true
-	} else {
-		return false
+func FileExists(filename string) (bool, error) {
+	_, err := os.Stat(filename)
+	
+	if err != nil {
+		if os.IsNotExist(err) { // File doesn't exists
+			return false, nil
+		} else {
+			return false, err // an error ocurred
+		}
 	}
+	
+	return true, nil
 }
 
 // Split filename and extension, where path = filename + extension.
@@ -24,13 +30,24 @@ func Splitext(path string) (string, string) {
 
 // Dealing with already existing files with the same name. Returns a string
 // with the Windows-popularized format of "filename (number).extension".
-func RepeatedFilenames(path string) string {
+func RepeatedFilenames(path string) (string, error) {
 	base, ext := Splitext(path)
 	files_this_name := 0
 	filename := fmt.Sprintf("%s%s", base, ext)
-	for FileExists(filename) {
+	
+	file_exists, err := FileExists(filename)
+	if err != nil {
+		return filename, err
+	}
+	
+	for file_exists{
 		files_this_name++
 		filename = fmt.Sprintf("%s (%d)%s", base, files_this_name, ext)
+		
+		file_exists, err = FileExists(filename)
+		if err != nil {
+			return "error", err
+		}
 	}
-	return filename
+	return filename, nil
 }
